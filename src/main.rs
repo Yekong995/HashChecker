8ad37d2cfb01c9e5528d6b528884c3783ab5d381
yekong995 <yekongyt@gmail.com>
2024-07-07 15:05:50 +0800

    Remove hashfile arg parser

--- src/main.rs
+++ src/main.rs
@@ -16,14 +16,9 @@ struct Args {
     #[arg(long, default_value = "sha256")]
     hash: String,
 
-    /// If this is provided, do a hash check,
-    /// you can provide a hash file to check against, or a hash string
+    /// If this is provided, do a hash check
     #[arg(short, long)]
     check: Option<String>,
-
-    /// Generate hash file, this will generate all supported hash
-    #[arg(short, long)]
-    generatehashfile: bool,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -32,43 +27,31 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     
     let filename = args.file;
 
-    if args.generatehashfile && is_file(filename.clone())? {
-        print!("Generating hash file...\r");
-        generate_hash_file(filename.clone())?;
-        println!("Hash file generated!");
-        return Ok(());
-    }
-
     if args.check.is_some() {
         let check = args.check.clone().unwrap().to_lowercase();
 
-        if is_file(check.clone())? {
-            check_with_hash_file(check.clone(), filename.clone())?;
+        let mut file = File::open(filename.clone())?;
+        let mut buffer = Vec::new();
+        file.read_to_end(&mut buffer)?;
+
+        let mut hasher = select_hasher(&args.hash);
+        let hash = use_hasher(&mut *hasher, &buffer);
+
+        let strs = hash
+            .iter()
+            .map(|b| format!("{:02x}", b))
+            .collect::<Vec<_>>()
+            .join("");
+        
+        println!("{}::{}", args.hash.to_uppercase(), strs);
+        println!("Hash you provided: {}", check);
+        
+        if strs == check {
+            println!("Hash check passed!");
         } else {
-
-            let mut file = File::open(filename.clone())?;
-            let mut buffer = Vec::new();
-            file.read_to_end(&mut buffer)?;
-
-            let mut hasher = select_hasher(&args.hash);
-            let hash = use_hasher(&mut *hasher, &buffer);
-
-            let strs = hash
-                .iter()
-                .map(|b| format!("{:02x}", b))
-                .collect::<Vec<_>>()
-                .join("");
-
-            if strs == check {
-                println!("{}::{}", args.hash.to_uppercase(), strs);
-                println!("Hash you provided: {}", check);
-                println!("Hash check passed!");
-            } else {
-                println!("{}::{}", args.hash.to_uppercase(), strs);
-                println!("Hash you provided: {}", check);
-                println!("Hash check failed!");
-            }
+            println!("Hash check failed!");
         }
+
     } else {
 
         let mut file = File::open(filename.clone())?;
